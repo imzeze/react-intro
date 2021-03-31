8878da1cf4e7ef3cfb9807125db7720553a0d5bf
jye019 <jihyekim019@gmail.com>
2021-03-31 13:40:43 +0900

    스타일 수정

--- src/components/organisms/section/Nav.tsx
+++ src/components/organisms/section/Nav.tsx
@@ -1,12 +1,22 @@
+import styled from 'styled-components'
 import { Flex } from '../../atoms'
 
 const Nav = () => {
   return (
-    <Flex justify="start">
+    <CustomNav justify="start">
       <div>홈</div>
       <div>게시판</div>
-    </Flex>
+    </CustomNav>
   )
 }
 
 export default Nav
+
+const CustomNav = styled(Flex)`
+  width: 100%;
+  margin: 20px;
+
+  > div {
+    padding: 20px;
+  }
+`
